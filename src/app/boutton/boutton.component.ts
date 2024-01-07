1e06180a895e7e5e779ac8b2e699d7a0ff25c2e6
Mohamed ABDELNABI <abdelnabi.mohamed.13015@gmail.com>
2024-01-07 04:59:50 +0100

    add tailwind

---
+++ src/app/boutton/boutton.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boutton',
+  standalone: true,
+  imports: [],
+  templateUrl: './boutton.component.html',
+  styleUrl: './boutton.component.scss'
+})
+export class BouttonComponent {
+
+}
